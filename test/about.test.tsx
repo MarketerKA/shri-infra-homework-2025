6bbd3830e9d3f3050ea73ef148498b2991f20d3c
Aidar Kadyrgulov <98264423+kadansama@users.noreply.github.com>
2025-07-18 17:05:26 +0300

    Update about.test.tsx

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: [Кирилл Архипов]');
+        expect(container.textContent).toBe('AboutAuthor: Кирилл Архипов');
     });
 });
